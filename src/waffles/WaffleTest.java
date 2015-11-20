b1bdb97e6fc7a784ccddf379f4aa8a1498408a23
Stephane Mikaty <stephane@mikaty.net>
2015-11-20 12:27:13 +0100

    initial checkin

---
+++ src/waffles/WaffleTest.java
@@ -0,0 +1,12 @@
+package waffles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaffleTest {
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
